f21e184c6154eb4f3ef745c744b0f4cc5d941f42
nat-1501 <natalisoares90@gmail.com>
2022-04-21 22:04:35 -0300

    test 2 passed

--- src/index.js
+++ src/index.js
@@ -1,25 +1,53 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
+// Middleware
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+        if (!user) {
+        return response.status(400).json({ error: "usuario não encontrado"});
+      }
+        request.user = user;
+        return next ();
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+      const { name, username } = request.body;
+
+      const usersAlreadyExists = users.some(
+        (user) => user.username === username
+      );
+
+      if (usersAlreadyExists) {
+        return response.status(400).json({ error: "Usuario já existe!"})
+      } 
+
+      const user = { 
+        name, 
+        username, 
+        id: uuidv4(), // precisa ser um uuid
+        todos: []
+      }
+      users.push(user);
+
+      return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
